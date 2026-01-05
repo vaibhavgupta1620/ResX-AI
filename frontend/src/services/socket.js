2191a10092f295c675ecd345f74b485e4ff00cd4
Vaibhav Gupta <vaibhavgupta1620@gmail.com>
2026-01-05 23:26:05 +0530

    Fix socket.io URL for production

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
-// Use backend URL from environment variable
 const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(SOCKET_URL, {
-    transports: ["websocket"],
+    transports: ["polling", "websocket"], // ✅ allow fallback
     withCredentials: true,
+    autoConnect: false, // ✅ VERY IMPORTANT
 });
